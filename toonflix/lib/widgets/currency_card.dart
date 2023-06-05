c3056c7551e0ba24c042e65643f6febc56bd7430
nakevin96 <nakevin96@hanyang.ac.kr>
2023-06-05 10:59:24 +0900

    Study: chapter03 complete

--- toonflix/lib/widgets/currency_card.dart
+++ toonflix/lib/widgets/currency_card.dart
@@ -6,6 +6,7 @@ class CurrencyCard extends StatelessWidget {
   final String currencyUnit;
   final IconData currencyIcon;
   final bool isInverted;
+  final double order;
 
   const CurrencyCard({
     super.key,
@@ -14,71 +15,75 @@ class CurrencyCard extends StatelessWidget {
     required this.currencyUnit,
     required this.currencyIcon,
     required this.isInverted,
+    required this.order,
   });
 
   @override
   Widget build(BuildContext context) {
     var mainThemeColor = isInverted ? Colors.white : const Color(0xFF1F2123);
     var subThemeColor = isInverted ? const Color(0xFF1F2123) : Colors.white;
-    return Container(
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-        color: mainThemeColor,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(28),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  currencyName,
-                  style: TextStyle(
+    return Transform.translate(
+      offset: Offset(0, -20 * order),
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          color: mainThemeColor,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(28),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    currencyName,
+                    style: TextStyle(
+                      color: subThemeColor,
+                      fontSize: 28,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        balance,
+                        style: TextStyle(
+                          color: subThemeColor,
+                          fontSize: 20,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        currencyUnit,
+                        style: TextStyle(
+                          color: subThemeColor.withOpacity(0.6),
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Transform.scale(
+                scale: 2.2,
+                child: Transform.translate(
+                  offset: const Offset(-4.0, 12.0),
+                  child: Icon(
+                    currencyIcon,
                     color: subThemeColor,
-                    fontSize: 28,
-                    fontWeight: FontWeight.w600,
+                    size: 88,
                   ),
                 ),
-                const SizedBox(height: 16),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      balance,
-                      style: TextStyle(
-                        color: subThemeColor,
-                        fontSize: 20,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    Text(
-                      currencyUnit,
-                      style: TextStyle(
-                        color: subThemeColor.withOpacity(0.6),
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Transform.scale(
-              scale: 2.2,
-              child: Transform.translate(
-                offset: const Offset(-4.0, 12.0),
-                child: Icon(
-                  currencyIcon,
-                  color: subThemeColor,
-                  size: 88,
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
